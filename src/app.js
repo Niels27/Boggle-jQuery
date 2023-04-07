76a559c43a494153692a6e580f4792c2b416ea39
Niels27 <nielsvdwal.27@gmail.com>
2023-04-07 23:53:09 +0200

    webpack 5+ babel finish

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import style from './main.css'
+import component from './component.js'
+
+console.log("hello world")
+
+//returns main element which will get appended to the body
+document.body.append(component())
+
+//jquery
+// $(document).ready(function(){
+
+
+
+//   });
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,43 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
     mode: "development", //production
     entry: {
-        main: path.resolve(__dirname, "src/js/app.js"),
+        main: path.resolve(__dirname, "src/app.js"),
 
     },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[contenthash].js",
+        assetModuleFilename: '[name][ext]',
         clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 5001,
+        open: true,
+        hot: true,
+        watchFiles: [path.resolve(__dirname, 'src')],
+
     },
     //loaders
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+            //css
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            //images
+            { test: /\.(svg|ico|png|jpg|jpeg|webp|gif)$/, type: 'asset/resource' },
+            //js for babel that are not in node_modules
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+
+            }
         ],
     },
     //plugins
